d253e0a2e3183d1a137be54243a2e313f0b24245
cleitoncfs <cleitoncfs@gmail.com>
2025-06-24 18:53:17 +0100

    update skills

--- src/components/SkillsContainer/SkillsContainer.jsx
+++ src/components/SkillsContainer/SkillsContainer.jsx
@@ -39,9 +39,18 @@ const SkillsContainer = () => {
                 <div className={styles.skillBox}>
                     <p className={styles.skillTitle}>Front-end</p>
                     <div className={styles.iconsContainer}>
-                        <i className="devicon-html5-plain colored"></i>
-                        <i className="devicon-css3-plain colored"></i>
-                        <i className="devicon-javascript-plain colored"></i>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-html5-plain colored"></i>
+                            <span className={styles.techLabel}>HTML5</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-css3-plain colored"></i>
+                            <span className={styles.techLabel}>CSS3</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-javascript-plain colored"></i>
+                            <span className={styles.techLabel}>JavaScript</span>
+                        </div>
                     </div>
                 </div>
 
@@ -49,9 +58,18 @@ const SkillsContainer = () => {
                 <div className={styles.skillBox}>
                     <p className={styles.skillTitle}>Back-end</p>
                     <div className={styles.iconsContainer}>
-                        <i className="devicon-nodejs-plain colored"></i>
-                        <i className="devicon-python-plain colored"></i>
-                        <i className="devicon-php-plain colored"></i>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-nodejs-plain colored"></i>
+                            <span className={styles.techLabel}>Node.js</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-python-plain colored"></i>
+                            <span className={styles.techLabel}>Python</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-php-plain colored"></i>
+                            <span className={styles.techLabel}>PHP</span>
+                        </div>
                     </div>
                 </div>
 
@@ -59,9 +77,18 @@ const SkillsContainer = () => {
                 <div className={styles.skillBox}>
                     <p className={styles.skillTitle}>Bancos de Dados</p>
                     <div className={styles.iconsContainer}>
-                        <i className="devicon-mysql-plain colored"></i>
-                        <i className="devicon-mongodb-plain colored"></i>
-                        <i className="devicon-sqlite-plain colored"></i>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-mysql-plain colored"></i>
+                            <span className={styles.techLabel}>MySQL</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-mongodb-plain colored"></i>
+                            <span className={styles.techLabel}>MongoDB</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-sqlite-plain colored"></i>
+                            <span className={styles.techLabel}>SQLite</span>
+                        </div>
                     </div>
                 </div>
 
@@ -69,9 +96,18 @@ const SkillsContainer = () => {
                 <div className={styles.skillBox}>
                     <p className={styles.skillTitle}>Frameworks Front-end</p>
                     <div className={styles.iconsContainer}>
-                        <i className="devicon-react-plain colored"></i>
-                        <i className="devicon-vuejs-plain colored"></i>
-                        <i className="devicon-angularjs-plain colored"></i>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-react-plain colored"></i>
+                            <span className={styles.techLabel}>React</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-vuejs-plain colored"></i>
+                            <span className={styles.techLabel}>Vue.js</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-angularjs-plain colored"></i>
+                            <span className={styles.techLabel}>AngularJS</span>
+                        </div>
                     </div>
                 </div>
 
@@ -79,9 +115,18 @@ const SkillsContainer = () => {
                 <div className={styles.skillBox}>
                     <p className={styles.skillTitle}>Frameworks Back-end</p>
                     <div className={styles.iconsContainer}>
-                        <i className="devicon-flask-plain colored"></i>
-                        <i className="devicon-django-plain colored"></i>
-                        <i className="devicon-laravel-plain colored"></i>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-flask-plain colored"></i>
+                            <span className={styles.techLabel}>Flask</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-django-plain colored"></i>
+                            <span className={styles.techLabel}>Django</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-laravel-plain colored"></i>
+                            <span className={styles.techLabel}>Laravel</span>
+                        </div>
                     </div>
                 </div>
 
@@ -89,9 +134,18 @@ const SkillsContainer = () => {
                 <div className={styles.skillBox}>
                     <p className={styles.skillTitle}>Ferramentas</p>
                     <div className={styles.iconsContainer}>
-                        <i className="devicon-git-plain colored"></i>
-                        <i className="devicon-linux-plain colored"></i>
-                        <i className="devicon-docker-plain colored"></i>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-git-plain colored"></i>
+                            <span className={styles.techLabel}>Git</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-linux-plain colored"></i>
+                            <span className={styles.techLabel}>Linux</span>
+                        </div>
+                        <div className={styles.iconWrapper}>
+                            <i className="devicon-docker-plain colored"></i>
+                            <span className={styles.techLabel}>Docker</span>
+                        </div>
                     </div>
                 </div>
             </div>
