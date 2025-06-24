b25fab09132442fd8677616410520b528ddf87ad
cleitoncfs <cleitoncfs@gmail.com>
2025-06-24 19:14:00 +0100

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,17 +8,38 @@ const Footer = () => {
                 © {new Date().getFullYear()} - Cleiton Santos. Todos os direitos
                 reservados.
             </p>
-            <p>
+
+            <nav className={styles.linksGroup}>
                 <a className={styles.link} href="#container">
-                    Voltar ao topo
-                </a>{" "}
-                |{" "}
+                    <i className="bi bi-arrow-up-circle"></i> Voltar ao topo
+                </a>
+                <a className={styles.link} href="mailto:cleitoncfs@gmail.com">
+                    <i className="bi bi-envelope-fill"></i> Contato
+                </a>
                 <a
                     className={styles.link}
                     href="https://github.com/cleitoncfs"
                     target="_blank"
+                    rel="noreferrer"
                 >
-                    GitHub
+                    <i className="bi bi-github"></i> GitHub
+                </a>
+                <a
+                    className={styles.link}
+                    href="https://www.linkedin.com/in/cleitoncfs/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <i className="bi bi-linkedin"></i> LinkedIn
+                </a>
+            </nav>
+
+            <p className={styles.builtWith}>
+                Desenvolvido com{" "}
+                <i className="devicon-react-original colored"></i> React &
+                hospedado na{" "}
+                <a href="https://netlify.com" target="_blank" rel="noreferrer">
+                    Netlify
                 </a>
             </p>
         </footer>
